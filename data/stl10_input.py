158c059e1ba04aefdd669fc120b6cd748e870f81
k3rx <kerx97@gmail.com>
2020-04-02 20:01:02 +0800

    Update stl10_input.py
    Added in some images function

--- data/stl10_input.py
+++ data/stl10_input.py
@@ -78,6 +78,33 @@ def read_all_images(path_to_data):
         images = np.transpose(images, (0, 3, 2, 1))
         return images
 
+def read_some_images(path_to_data, n):
+    """
+    :param path_to_data: the file containing the binary images from the STL-10 dataset
+    :return: an array containing all the images
+    """
+
+    with open(path_to_data, 'rb') as f:
+        # read whole file in uint8 chunks
+        everything = np.fromfile(f, dtype=np.uint8, count = n * SIZE)
+
+        # We force the data into 3x96x96 chunks, since the
+        # images are stored in "column-major order", meaning
+        # that "the first 96*96 values are the red channel,
+        # the next 96*96 are green, and the last are blue."
+        # The -1 is since the size of the pictures depends
+        # on the input file, and this way numpy determines
+        # the size on its own.
+
+        images = np.reshape(everything, (-1, 3, 96, 96))
+
+        # Now transpose the images into a standard image format
+        # readable by, for example, matplotlib.imshow
+        # You might want to comment this line or reverse the shuffle
+        # if you will use a learning algorithm like CNN, since they like
+        # their channels separated.
+        images = np.transpose(images, (0, 3, 2, 1))
+        return images
 
 def read_single_image(image_file):
     """
