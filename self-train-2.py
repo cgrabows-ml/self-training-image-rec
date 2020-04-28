60aa11df940a393b318b12915c385303093f7f12
k3rx <kerx97@gmail.com>
2020-04-28 14:51:49 -0400

    Update self-train-2.py

--- self-train-2.py
+++ self-train-2.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
+from keras import regularizers
 
 
 from data.stl10_input import read_labels, read_all_images, read_some_images
@@ -23,6 +24,7 @@ def main(argv):
 
     opts, args = getopt.getopt(argv, "hi:o:", ["test=", "experiment=", "subset=", "self_train=", "epochs=", "batch=", "threshold="])
 
+    # Setting Defaults:
     self_train = False
     subset_size = 1
     epochs = 15
@@ -31,6 +33,8 @@ def main(argv):
     experiment = "default"
     binary_classification = False
 
+
+    # Reading Options
     for opt, arg in opts:
         if opt == '-h':
             print('self-train.py --experiment=binary --subset=1 --self_train=True --epochs=20 --batch=1000 --threshold=.75')
@@ -51,8 +55,8 @@ def main(argv):
         elif opt in ("-t", "--threshold"):
             threshold = float(arg)
 
-    print(self_train)
-    print(subset_size)
+    print("Self Train: ", self_train)
+    print("Subset Size: ", subset_size)
     IMG_HEIGHT = 96
     IMG_WIDTH = 96
 
@@ -75,12 +79,18 @@ def main(argv):
         save_weights_only=True,
         period=5)
 
-    print(len(test_labels))
-    print(len(train_labels))
-    test_labels, test_images = convert_labels(test_labels, test_images, experiment)
-    train_labels, train_images = convert_labels(train_labels, train_images, experiment)
-    print(test_labels.shape)
-    print(train_labels.shape)
+    print("Test Labels: ", len(test_labels))
+    print("Train Labels: ", len(train_labels))
+
+    # Converting labels - we split to 3 label sets with disagreement
+    if experiment == "disagreement":
+        # FILL ME IN
+        # 3 label sets? or just 1 and then additional code later
+    else:
+        test_labels, test_images = convert_labels(test_labels, test_images, experiment)
+        train_labels, train_images = convert_labels(train_labels, train_images, experiment)
+    print("Test Label Shape: ", test_labels.shape)
+    print("Test Label Shape: ", train_labels.shape)
     # exit()
 
 
@@ -117,9 +127,13 @@ def main(argv):
     else:
         num_classes = 10
 
+
+    # Defining Model: Fill in improved model here
+
     model = Sequential([
         Conv2D(16, 3, padding='same', activation='relu',
-               input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+               input_shape=(IMG_HEIGHT, IMG_WIDTH ,3),
+               kernel_regularizer=regularizers.l2(0.01)),
         MaxPooling2D(),
         Dropout(0.2),
         Conv2D(32, 3, padding='same', activation='relu'),
@@ -132,9 +146,51 @@ def main(argv):
         Dense(num_classes)
     ])
 
+    if experiment == "disagreement":
+        Amodel = Sequential([
+            Conv2D(16, 3, padding='same', activation='relu',
+                   input_shape=(IMG_HEIGHT, IMG_WIDTH ,3),
+                   kernel_regularizer=regularizers.l2(0.01)),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Conv2D(32, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Conv2D(64, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Flatten(),
+            Dense(512, activation='relu'),
+            Dense(7)
+        ])
+        Mmodel = Sequential([
+            Conv2D(16, 3, padding='same', activation='relu',
+                   input_shape=(IMG_HEIGHT, IMG_WIDTH ,3),
+                   kernel_regularizer=regularizers.l2(0.01)),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Conv2D(32, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Conv2D(64, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Flatten(),
+            Dense(512, activation='relu'),
+            Dense(5)
+        ])
+
     if experiment=="binary":
         model.compile(optimizer='adam',
                       loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+    elif experiment == "disagreement":
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+        Amodel.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+        Mmodel.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                       metrics=['accuracy'])
     else:
         model.compile(optimizer='adam',
@@ -152,30 +208,129 @@ def main(argv):
         horizontal_flip=True,
         fill_mode="nearest")
 
-    if not self_train:
-        history = model.fit(
-            aug.flow(train_images, train_labels, batch_size = batch_size),
-            steps_per_epoch=num_train // batch_size,
-            epochs=epochs,
-            callbacks=[cp_callback],
-            validation_data=(test_images, test_labels),
-            validation_steps=num_test // batch_size
-        )
+    if experiment == "disagreement":
+        # Single loop.
+        if not self_train:
+            # history = model.fit(
+            #     aug.flow(train_images, train_labels, batch_size = batch_size),
+            #     steps_per_epoch=num_train // batch_size,
+            #     epochs=epochs,
+            #     callbacks=[cp_callback],
+            #     validation_data=(test_images, test_labels),
+            #     validation_steps=num_test // batch_size
+            # )
+            Ahistory = Amodel.fit(
+                aug.flow(train_images, train_labels, batch_size = batch_size),
+                steps_per_epoch=num_train // batch_size,
+                epochs=epochs,
+                callbacks=[cp_callback],
+                validation_data=(test_images, test_labels),
+                validation_steps=num_test // batch_size
+            )
+            Mhistory = Mmodel.fit(
+                aug.flow(train_images, train_labels, batch_size = batch_size),
+                steps_per_epoch=num_train // batch_size,
+                epochs=epochs,
+                callbacks=[cp_callback],
+                validation_data=(test_images, test_labels),
+                validation_steps=num_test // batch_size
+            )
+        ## Augment dataset with unlabeled images
+
+        if self_train:
+            # Reading only 1000 images for lack of computing power
+            #unlabeled_images = read_all_images(binary_path + "unlabeled_X.bin")
+            unlabeled_images = read_some_images(binary_path + "unlabeled_X.bin",
+                                                int(100000 * subset_size))
+            confident = True
+            # While there are still confident labelings
+            while confident:
+                # First train supervised model
+                print("Train images and Label shape: ",
+                      train_images.shape, train_labels.shape)
+                # history = model.fit(
+                #     aug.flow(train_images, train_labels, batch_size = batch_size),
+                #     steps_per_epoch=num_train // batch_size,
+                #     epochs=epochs,
+                #     callbacks=[cp_callback],
+                #     validation_data=(test_images, test_labels),
+                #     validation_steps=num_test // batch_size
+                # )
+                Ahistory = Amodel.fit(
+                    aug.flow(train_images, train_labels, batch_size = batch_size),
+                    steps_per_epoch=num_train // batch_size,
+                    epochs=epochs,
+                    callbacks=[cp_callback],
+                    validation_data=(test_images, test_labels),
+                    validation_steps=num_test // batch_size
+                )
+                Mhistory = Mmodel.fit(
+                    aug.flow(train_images, train_labels, batch_size = batch_size),
+                    steps_per_epoch=num_train // batch_size,
+                    epochs=epochs,
+                    callbacks=[cp_callback],
+                    validation_data=(test_images, test_labels),
+                    validation_steps=num_test // batch_size
+                )
+                # print(train_images[0])
 
 
+                # Predict unlabeled examples and add confident ones
+                count = 0
+                if (len(unlabeled_images) == 0): break
+                unlabeled_predictionsA = Amodel.predict(unlabeled_images)
+                unlabeled_predictionsM = Mmodel.predict(unlabeled_images)
+                # Converting this to probabilities:
+                # print(unlabeled_predictions, unlabeled_predictions.shape)
+                probs_A = tf.nn.softmax(unlabeled_predictionsA)
+                probs_M = tf.nn.softmax(unlabeled_predictionsM)
+                temp_labA = np.argmax(probs_A, axis = 1)
+                temp_labM = np.argmax(probs_M, axis = 1)
 
-    ## Augment dataset with unlabeled images
+                # Combining animal and machine labels to one
+                animal_labels = [2,4,5,6,7,8,0]
+                machine_labels = [1,3,9,10,0]
+                # If both predicts other or both predict non-other,
+                # it should not get selected for ST so wrong labels wont matter
+                new_labels = []
+                for i in range(len(temp_labA)):
+                    new_labels.append(max(animal_labels[temp_labA[i]],
+                                          machine_labels[temp_labM[i]]))
+                class_predsA = np.amax(probs_A, axis = 1)
+                class_predsM = np.amax(probs_M, axis = 1)
+                class_predsA = tf.reshape(class_predsA, [len(class_predsA)])
+                class_predsM = tf.reshape(class_predsM, [len(class_predsM)])
+                class_preds = class_predsA * class_predsM
 
-    if self_train:
-        # Reading only 1000 images for lack of computing power
-        #unlabeled_images = read_all_images(binary_path + "unlabeled_X.bin")
-        unlabeled_images = read_some_images(binary_path + "unlabeled_X.bin",
-                                            int(100000 * subset_size))
-        confident = True
-        # While there are still confident labelings
-        while confident:
-            # First train supervised model
-            print(train_images.shape, train_labels.shape)
+                assert class_preds.shape == class_predsA.shape
+
+                to_remove = (class_preds >= threshold) and
+                    ((temp_labA == 6) != (temp_labM == 4))
+                    # XOR: one and only one of the predictions is 'other'
+                train_images = np.append(train_images, unlabeled_images[to_remove])
+                train_labels = np.append(train_labels, new_labels[to_remove])
+                count = np.sum(to_remove)
+                unlabeled_images = unlabeled_images[np.logical_not(to_remove)]
+                print(count)
+                print(unlabeled_images.shape)
+
+                #train_images = train_images.reshape(-1, 3, 96, 96)
+                #train_images = np.transpose(train_images, (0, 3, 2, 1))
+                train_images = train_images.reshape(-1, 96, 96, 3)
+                #print(train_images[0])
+
+                # Recalculating num_train and batch_size:
+                num_train = len(train_labels)
+                num_batches = math.ceil(num_train/batch_size)
+                batch_size = int(num_train/num_batches)
+
+                # No confident labelings left
+                if count == 0: #< 50:
+                    confident = False
+                    print("Exiting loop")
+    else:
+        # Single loop.
+        if not self_train:
             history = model.fit(
                 aug.flow(train_images, train_labels, batch_size = batch_size),
                 steps_per_epoch=num_train // batch_size,
@@ -185,60 +340,88 @@ def main(argv):
                 validation_steps=num_test // batch_size
             )
 
-            # Predict unlabeled examples and add confident ones
-            count = 0
-            if (len(unlabeled_images) == 0): break
-            unlabeled_predictions = model.predict(unlabeled_images)
-            # Converting this to probabilities:
-            temp_lab = -1
-            if experiment != "binary":
-                probss = tf.nn.softmax(unlabeled_predictions)
-                class_preds = np.amax(probss, axis = 1)
-                temp_lab = np.argmax(probss, axis = 1)
-            else:
-                # print(unlabeled_predictions, unlabeled_predictions.shape)
-                class_preds = tf.nn.sigmoid(unlabeled_predictions)
-                temp_lab = tf.round(class_preds)
-            class_preds = tf.reshape(class_preds, [len(class_preds)])
+        ## Augment dataset with unlabeled images
 
-            # Version 1: Not vectorized
-            # to_keep = list(range(len(class_preds)))
-            # for i in range(len(class_preds)):
-            #     if i % 50 == 0:
-            #         print("Augmenting dataset " + str(i) + "/" + str(len(unlabeled_images)) + " complete")
-            #     pred = class_preds[i]
-            #     image = unlabeled_images[i]
-            #     if np.amax(pred) >= threshold:
-            #         train_images = np.append(train_images, image)
-            #         train_labels = np.append(train_labels, temp_lab[i])
-            #         # decrease size of unlabeled images
-            #         to_keep.remove(i)
-            #         count += 1
-            #unlabeled_images = unlabeled_images[to_keep, :, :, :]
+        if self_train:
+            # Reading only 1000 images for lack of computing power
+            #unlabeled_images = read_all_images(binary_path + "unlabeled_X.bin")
+            unlabeled_images = read_some_images(binary_path + "unlabeled_X.bin",
+                                                int(100000 * subset_size))
+            confident = True
+            # While there are still confident labelings
+            while confident:
+                # First train supervised model
+                print("Train images and Label shape: ",
+                      train_images.shape, train_labels.shape)
+                history = model.fit(
+                    aug.flow(train_images, train_labels, batch_size = batch_size),
+                    steps_per_epoch=num_train // batch_size,
+                    epochs=epochs,
+                    callbacks=[cp_callback],
+                    validation_data=(test_images, test_labels),
+                    validation_steps=num_test // batch_size
+                )
+               # print(train_images[0])
+                # Predict unlabeled examples and add confident ones
+                count = 0
+                if (len(unlabeled_images) == 0): break
+                unlabeled_predictions = model.predict(unlabeled_images)
+                # Converting this to probabilities:
+                temp_lab = -1
+                if experiment != "binary":
+                    probss = tf.nn.softmax(unlabeled_predictions)
+                    class_preds = np.amax(probss, axis = 1)
+                    temp_lab = np.argmax(probss, axis = 1)
+                else:
+                    # print(unlabeled_predictions, unlabeled_predictions.shape)
+                    class_preds = tf.nn.sigmoid(unlabeled_predictions)
+                    temp_lab = tf.round(class_preds)
+                class_preds = tf.reshape(class_preds, [len(class_preds)])
 
-            # Version 2: Vectorized?
-            to_remove = class_preds >= threshold
-            train_images = np.append(train_images, unlabeled_images[to_remove])
-            train_labels = np.append(train_labels, temp_lab[to_remove])
-            count = np.sum(to_remove)
-            unlabeled_images = unlabeled_images[np.logical_not(to_remove)]
-            print(count)
-            print(unlabeled_images.shape)
+                # Version 1: Not vectorized
+                # to_keep = list(range(len(class_preds)))
+                # for i in range(len(class_preds)):
+                #     if i % 50 == 0:
+                #         print("Augmenting dataset " + str(i) + "/" + str(len(unlabeled_images)) + " complete")
+                #     pred = class_preds[i]
+                #     image = unlabeled_images[i]
+                #     if np.amax(pred) >= threshold:
+                #         train_images = np.append(train_images, image)
+                #         train_labels = np.append(train_labels, temp_lab[i])
+                #         # decrease size of unlabeled images
+                #         to_keep.remove(i)
+                #         count += 1
+                #unlabeled_images = unlabeled_images[to_keep, :, :, :]
 
-            train_images = train_images.reshape(-1, 3, 96, 96)
-            train_images = np.transpose(train_images, (0, 3, 2, 1))
-            # Recalculating num_train and batch_size:
-            num_train = len(train_labels)
-            num_batches = math.ceil(num_train/batch_size)
-            batch_size = int(num_train/num_batches)
+                # Version 2: Vectorized?
+                to_remove = class_preds >= threshold
+                train_images = np.append(train_images, unlabeled_images[to_remove])
+                train_labels = np.append(train_labels, temp_lab[to_remove])
+                count = np.sum(to_remove)
+                unlabeled_images = unlabeled_images[np.logical_not(to_remove)]
+                print(count)
+                print(unlabeled_images.shape)
 
-            # No confident labelings left
-            if count == 0: #< 50:
-                confident = False
-                print("Exiting loop")
+                #train_images = train_images.reshape(-1, 3, 96, 96)
+                #train_images = np.transpose(train_images, (0, 3, 2, 1))
+                train_images = train_images.reshape(-1, 96, 96, 3)
+                #print(train_images[0])
+                # Recalculating num_train and batch_size:
+                num_train = len(train_labels)
+                num_batches = math.ceil(num_train/batch_size)
+                batch_size = int(num_train/num_batches)
+
+                # No confident labelings left
+                if count == 0: #< 50:
+                    confident = False
+                    print("Exiting loop")
     model.save_weights(checkpoint_path.format(epoch=0))
     # model.save(expe)
 
+
+
+
+    # TO DO: computing accuracy of model and loss of model
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
 
