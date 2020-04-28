17252545cd77cfcb52af6b6e53aec3d87f158478
Macetodaface <cgrabows@andrew.cmu.edu>
2020-04-28 16:08:04 -0400

    Started disagreement

--- self-train.py
+++ self-train.py
@@ -56,142 +56,214 @@ def main(argv):
 
     binary_path = "data/stl10_binary/"
 
-    train_images = read_all_images(binary_path + "train_X.bin")
-    train_labels = read_labels(binary_path + "train_y.bin")
+    # if experiment != "disagreement":
 
-    test_images = read_all_images(binary_path + "test_X.bin")
-    test_labels = read_labels(binary_path + "test_y.bin")
+    og_train_images = read_all_images(binary_path + "train_X.bin")
+    og_train_labels = read_labels(binary_path + "train_y.bin")
 
+    og_test_images = read_all_images(binary_path + "test_X.bin")
+    og_test_labels = read_labels(binary_path + "test_y.bin")
 
-    checkpoint_path = "training/" + experiment + "-cp-{epoch:04d}.ckpt"
-    checkpoint_dir = os.path.dirname(checkpoint_path)
-
-    # Create a callback that saves the model's weights every 5 epochs
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path,
-        verbose=1,
-        save_weights_only=True,
-        period=5)
-
-    print(len(test_labels))
-    print(len(train_labels))
-    test_labels, test_images = convert_labels(test_labels, test_images, experiment)
-    train_labels, train_images = convert_labels(train_labels, train_images, experiment)
-    print(test_labels.shape)
-    print(train_labels.shape)
-    # exit()
-
-
-    num_train = math.floor(len(train_labels)*subset_size)
-    num_test = math.floor(len(test_labels)*subset_size)
-
-    test_labels = test_labels[:num_test]
-    train_labels = train_labels[:num_train]
-    test_images = test_images[:num_test]
-    train_images = train_images[:num_train]
-
-    num_batches = math.ceil(num_train/batch_size)
-    batch_size = int(num_train/num_batches)
-
-    print("Adjusted batch size to " + str(batch_size))
-
-    def plotImages(images_arr):
-        fig, axes = plt.subplots(1, 5, figsize=(20,20))
-        axes = axes.flatten()
-        for img, ax in zip( images_arr, axes):
-            ax.imshow(img)
-            ax.axis('off')
-        plt.tight_layout()
-        plt.show()
-
-    #plotImages(train_images[:5])
-
-    if experiment == "binary":
-        num_classes = 1
-    elif experiment == "animal":
-        num_classes = 6
-    elif experiment == "machine":
-        num_classes = 4
+    experiments = []
+    if experiment == "disagreement":
+        experiments = ["disagreement_animal", "disagreement_machine"]
     else:
-        num_classes = 10
+        experiments = [experiment]
 
-    model = Sequential([
-        Conv2D(16, 3, padding='same', activation='relu',
-               input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-        MaxPooling2D(),
-        Dropout(0.2),
-        Conv2D(32, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Conv2D(64, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Dropout(0.2),
-        Flatten(),
-        Dense(512, activation='relu'),
-        Dense(num_classes)
-    ])
+    models = []
 
-    if experiment=="binary":
-        model.compile(optimizer='adam',
-                      loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                      metrics=['accuracy'])
-    else:
-        model.compile(optimizer='adam',
-                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                      metrics=['accuracy'])
+    for sub_experiment in experiments:
 
-    model.summary()
+        checkpoint_path = "training/" + experiment + "-cp-{epoch:04d}.ckpt"
+        checkpoint_dir = os.path.dirname(checkpoint_path)
 
-    aug = ImageDataGenerator(
-        rotation_range=30,
-        zoom_range=0.15,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.15,
-        horizontal_flip=True,
-        fill_mode="nearest")
+        # Create a callback that saves the model's weights every 5 epochs
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_path,
+            verbose=1,
+            save_weights_only=True,
+            period=5)
 
-    history = model.fit(
-        aug.flow(train_images, train_labels, batch_size = batch_size),
-        steps_per_epoch=num_train // batch_size,
-        epochs=epochs,
-        callbacks=[cp_callback],
-        validation_data=(test_images, test_labels),
-        validation_steps=num_test // batch_size
-    )
+        test_labels, test_images = convert_labels(og_test_labels, og_test_images, sub_experiment)
+        train_labels, train_images = convert_labels(og_train_labels, og_train_images, sub_experiment)
 
-    model.save_weights(checkpoint_path.format(epoch=0))
+        num_train = math.floor(len(train_labels)*subset_size)
+        num_test = math.floor(len(test_labels)*subset_size)
+
+        test_labels = test_labels[:num_test]
+        train_labels = train_labels[:num_train]
+        test_images = test_images[:num_test]
+        train_images = train_images[:num_train]
+
+        print(num_train, batch_size)
+
+        num_batches = math.ceil(num_train/batch_size)
+        batch_size = int(num_train/num_batches)
+
+        print("Adjusted batch size to " + str(batch_size))
+
+        if sub_experiment == "binary":
+            num_classes = 1
+        elif sub_experiment == "animal":
+            num_classes = 6
+        elif sub_experiment == "machine":
+            num_classes = 4
+        elif sub_experiment == "disagreement_animal":
+            num_classes = 7
+        elif sub_experiment == "disagreement_machine":
+            num_classes = 5
+        else:
+            num_classes = 10
 
 
-    ## Augment dataset with unlabeled images
+        model = Sequential([
+            Conv2D(16, 3, padding='same', activation='relu',
+                   input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Conv2D(32, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Conv2D(64, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Dropout(0.2),
+            Flatten(),
+            Dense(512, activation='relu'),
+            Dense(num_classes)
+        ])
 
-    if self_train:
-        unlabeled_images = read_all_images(binary_path + "unlabeled_X.bin")
-        unlabeled_predictions = model.predict(unlabeled_images)
-        for i in range(len(unlabeled_predictions)):
-            if i % 50 == 0:
-                print("Augmenting dataset " + str(i) + "/" + str(len(unlabeled_images)) + " complete")
-            pred = unlabeled_predictions[i]
-            image = unlabeled_images[i]
-            if np.amax(pred) >= .75:
-                train_images = np.append(train_images, image)
-                train_labels = np.append(train_labels, np.argmax(pred))
-                # train_images.append(image)
-                # converted_train_labels.append(np.argmax(pred))
+        if experiment=="binary":
+            model.compile(optimizer='adam',
+                          loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                          metrics=['accuracy'])
+        else:
+            model.compile(optimizer='adam',
+                          loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                          metrics=['accuracy'])
+
+        model.summary()
+
+        # aug = ImageDataGenerator(
+        #     rotation_range=30,
+        #     zoom_range=0.15,
+        #     width_shift_range=0.2,
+        #     height_shift_range=0.2,
+        #     shear_range=0.15,
+        #     horizontal_flip=True,
+        #     fill_mode="nearest")
 
         history = model.fit(
-            aug.flow(train_images, train_labels, batch_size = batch_size),
+            # fixed
+            train_images, train_labels,
+            # broken
+            # aug.flow(train_images, train_labels, batch_size = batch_size),
             steps_per_epoch=num_train // batch_size,
             epochs=epochs,
+            callbacks=[cp_callback],
             validation_data=(test_images, test_labels),
             validation_steps=num_test // batch_size
         )
 
-    # model.save(expe)
+        if experiment == "disagreement":
+            model.save_weights((checkpoint_path + sub_experiment).format(epoch=0))
+            models.append(model)
+        else:
+            model.save_weights(checkpoint_path.format(epoch=0))
+            models.append(model)
+
+
+    ## Augment dataset with unlabeled images
+    animal_labels = [2,4,5,6,7,8]
+    machine_labels = [1,3,9,10]
+    animal_model = models[0]
+    machine_model = models[1]
+
+    if self_train:
+        unlabeled_images = read_all_images(binary_path + "unlabeled_X.bin")
+        if experiment == "disagreement":
+            animal_predictions = animal_model.predict(unlabeled_images)
+            machine_predictions = machine_model.predict(unlabeled_images)
+            for i in range(len(unlabeled_images)):
+                best_pred = 0
+                pred = -1
+                for j in range(10):
+                    label = j+1
+                    if label in animal_labels:
+                        possible_pred = animal_predictions[i][animal_labels.index(label)]*machine_predictions[len(machine_labels)]
+                    elif label in machine_labels:
+                        possible_pred = machine_predictions[i][machine_labels.index(label)]*animal_predictions[len(animal_labels)]
+                    if possible_pred > best_pred:
+                        best_pred = possible_pred
+                        pred = label
+
+                if i % 50 == 0:
+                    print("Augmenting dataset " + str(i) + "/" + str(len(unlabeled_images)) + " complete")
+                # pred = unlabeled_predictions[i]
+                image = unlabeled_images[i]
+                if best_pred >= .75:
+                    train_images = np.append(train_images, image)
+                    # train_labels = np.append(train_labels, np.argmax(pred))
+                    animal_train_labels = np.append(animal_train_labels, pred)
+                    machine_train_labels = np.append(machine_train_labels, pred)
+                    # train_images.append(image)
+                    # converted_train_labels.append(np.argmax(pred))
+
+            animal_history = animal_model.fit(
+                # fixed
+                train_images, animal_train_labels,
+                # broken
+                # aug.flow(train_images, train_labels, batch_size = batch_size),
+                steps_per_epoch=num_train // batch_size,
+                epochs=epochs,
+                validation_data=(test_images, test_labels),
+                validation_steps=num_test // batch_size
+            )
+            animal_model.save_weights((checkpoint_path + "disagreement_animal").format(epoch=0))
+
+            machine_history = animal_model.fit(
+                # fixed
+                train_images, animal_train_labels,
+                # broken
+                # aug.flow(train_images, train_labels, batch_size = batch_size),
+                steps_per_epoch=num_train // batch_size,
+                epochs=epochs,
+                validation_data=(test_images, test_labels),
+                validation_steps=num_test // batch_size
+            )
+            machine_model.save_weights((checkpoint_path + "disagreement_machine").format(epoch=0))
+
+
+        else:
+            unlabeled_predictions = model.predict(unlabeled_images)
+            for i in range(len(unlabeled_predictions)):
+                if i % 50 == 0:
+                    print("Augmenting dataset " + str(i) + "/" + str(len(unlabeled_images)) + " complete")
+                pred = unlabeled_predictions[i]
+                image = unlabeled_images[i]
+                if np.amax(pred) >= .75:
+                    train_images = np.append(train_images, image)
+                    train_labels = np.append(train_labels, np.argmax(pred))
+                    # train_images.append(image)
+                    # converted_train_labels.append(np.argmax(pred))
+
+            history = model.fit(
+                # fixed
+                train_images,train_labels,
+                # broken
+                # aug.flow(train_images, train_labels, batch_size = batch_size),
+                steps_per_epoch=num_train // batch_size,
+                epochs=epochs,
+                validation_data=(test_images, test_labels),
+                validation_steps=num_test // batch_size
+            )
+            model.save_weights(checkpoint_path.format(epoch=0))
+
+
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
 
     loss=history.history['loss']
+
     val_loss=history.history['val_loss']
 
     epochs_range = range(epochs)
